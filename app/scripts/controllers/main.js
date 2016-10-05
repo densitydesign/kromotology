54e119341d7568e3fee65f57deea835471826f7e
iosonosempreio <tommaso.elli@gmail.com>
2016-10-05 22:19:50 +0200

    Asynchronous call
    Rewritten function that calls the backend in a way that is asynchronous
    and recursive. In this way the server that receives the calls is not
    overloaded with too many images in a single time.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -33,19 +33,62 @@ angular.module('kromotologyApp')
 	});
 
 	$scope.addRandomUrl = function() {
-		$scope.urlList = 'http://s1.picswalls.com/wallpapers/2014/06/18/free-vintage-cars-wallpaper_054104521_44.jpg\nhttp://wallpoper.com/images/00/33/25/75/cars-classic_00332575.jpg\nhttp://www.iloveindia.com/cars/pics/vintage-cars.jpg\nhttp://www.carwallpapershq.com/wp-content/uploads/2016/02/classic_cars_photo_hq.jpg\nhttp://www.motory.com/tinymce/Classic-Cars-11.jpg\nhttp://mixhdwallpapers.com/wp-content/uploads/2014/06/77.jpg\nhttps://mycamerajournal.files.wordpress.com/2013/03/rodrun-2.jpg\nhttps://upload.wikimedia.org/wikipedia/commons/thumb/1/1b/1925_Flint.jpg/250px-1925_Flint.jpg\nhttp://www.vintagecarsandbikes.in/image/bnr1.jpg\nhttp://www.hybridlava.com/wp-content/uploads/Vintage_cars_019.jpg\nhttp://gearpatrol.com/wp-content/uploads/2016/06/vintage-car-under-15k-gear-patrol-full-lead.jpg\nhttps://s-media-cache-ak0.pinimg.com/originals/9e/a6/14/9ea6146f13769794007b6a947bd3a903.jpg\nhttp://hd.wallpaperswide.com/thumbs/classic_jaguar_xj6-t1.jpg\nhttp://media1.santabanta.com/full1/Cars/Vintage and Classic Cars/vintage-and-classic-cars-79a.jpg\nhttp://randomwallpapers.net/vintage-cars-retro-1920x1080-wallpaper524186.jpg\nhttp://bpc.h-cdn.co/assets/16/14/980x490/landscape-1460048341-porsche-356-classic.jpg\nhttp://assets.bauer-wolke.co.uk/imagegen/p/152/114/s3/digital-cougar-assets-uk/MomoAds/2016/09/23/103247/Singer_BJL356_1.jpg\nhttp://www.queensofvintage.com/wp-content/uploads/2013/10/HMP_5312-2.jpg\nhttp://s4.weddbook.com/t4/8/8/9/889147/vintage-cars.jpg\nhttp://www.magic4walls.com/wp-content/uploads/2013/12/beautiful-vintage-car-108642.jpg\nhttps://s-media-cache-ak0.pinimg.com/originals/e2/02/38/e20238b7d3dc4b7f9809db80225df9fb.jpg\nhttp://cdn.wallpapersafari.com/31/30/JMlaV5.jpg\nhttp://assets.bauer-wolke.co.uk/imagegen/p/152/114/s3/digital-cougar-assets-uk/MomoAds/2016/09/23/105946/1.JPG\nhttp://skyryedesign.com/wp-content/uploads/2016/01/The-Best-Vintage-Car-Wallpapers-22-Best-Vintage-Car-wv-aston-martin-ferarri.jpg'
+		$scope.urlList = 'http://wallpoper.com/images/00/33/25/75/cars-classic_00332575.jpg\nhttp://www.iloveindia.com/cars/pics/vintage-cars.jpg\nhttp://www.carwallpapershq.com/wp-content/uploads/2016/02/classic_cars_photo_hq.jpg\nhttp://s1.picswalls.com/wallpapers/2014/06/18/free-vintage-cars-wallpaper_054104521_44.jpg\nhttp://www.motory.com/tinymce/Classic-Cars-11.jpg\nhttp://mixhdwallpapers.com/wp-content/uploads/2014/06/77.jpg\nhttps://mycamerajournal.files.wordpress.com/2013/03/rodrun-2.jpg\nhttps://upload.wikimedia.org/wikipedia/commons/thumb/1/1b/1925_Flint.jpg/250px-1925_Flint.jpg\nhttp://www.vintagecarsandbikes.in/image/bnr1.jpg\nhttp://www.hybridlava.com/wp-content/uploads/Vintage_cars_019.jpg\nhttp://gearpatrol.com/wp-content/uploads/2016/06/vintage-car-under-15k-gear-patrol-full-lead.jpg\nhttps://s-media-cache-ak0.pinimg.com/originals/9e/a6/14/9ea6146f13769794007b6a947bd3a903.jpg\nhttp://hd.wallpaperswide.com/thumbs/classic_jaguar_xj6-t1.jpg\nhttp://media1.santabanta.com/full1/Cars/Vintage and Classic Cars/vintage-and-classic-cars-79a.jpg\nhttp://randomwallpapers.net/vintage-cars-retro-1920x1080-wallpaper524186.jpg\nhttp://bpc.h-cdn.co/assets/16/14/980x490/landscape-1460048341-porsche-356-classic.jpg\nhttp://assets.bauer-wolke.co.uk/imagegen/p/152/114/s3/digital-cougar-assets-uk/MomoAds/2016/09/23/103247/Singer_BJL356_1.jpg\nhttp://www.queensofvintage.com/wp-content/uploads/2013/10/HMP_5312-2.jpg\nhttp://s4.weddbook.com/t4/8/8/9/889147/vintage-cars.jpg\nhttp://www.magic4walls.com/wp-content/uploads/2013/12/beautiful-vintage-car-108642.jpg\nhttps://s-media-cache-ak0.pinimg.com/originals/e2/02/38/e20238b7d3dc4b7f9809db80225df9fb.jpg\nhttp://cdn.wallpapersafari.com/31/30/JMlaV5.jpg\nhttp://assets.bauer-wolke.co.uk/imagegen/p/152/114/s3/digital-cougar-assets-uk/MomoAds/2016/09/23/105946/1.JPG\nhttp://skyryedesign.com/wp-content/uploads/2016/01/The-Best-Vintage-Car-Wallpapers-22-Best-Vintage-Car-wv-aston-martin-ferarri.jpg'
 	}
 
 	$scope.fetchColors = function(){
 		
 		// http://131.175.56.235:8080/single?img=http://stephenplusplus.github.io/yeoman.io/assets/img/yeoman-logo.png&k=5
 		$scope.listToParse = $scope.urlList.split('\n')
+
 		$scope.parsedImages = []
 		$scope.errors = 0;
 
-		$scope.listToParse.forEach(function(url,i){
-			var imageAddress = url
+		var position = 0;
+		$scope.recursiveFetch(position)
+
+		
+		// Old asyncronous function for the API call
+		// $scope.listToParse.forEach(function(url,i){
+		// 	var imageAddress = url
+		// 	var callString = "http://131.175.56.235:8080/single?img="+imageAddress+"&k=5"
+
+		// 	$http.get(callString)
+		// 		.then(function(response){
+		// 			response.data.id = i
+		// 			response.data.parsed = true
+		// 			$scope.parsedImages.push(response.data)
+		// 			$scope.parsedImages.sort(function(a,b) { return a.id - b.id; })
+		// 			if($scope.parsedImages.length > 0 && $scope.showElements.download == false) {
+		// 				$scope.showElements.download = true
+		// 				$scope.showElements.go = false
+		// 			}
+		// 		}, function(error){
+		// 			console.log('error for this url: ', url)
+		// 			$scope.errors++
+		// 			error.config.id = i
+		// 			error.config.parsed = false
+		// 			error.config.apiCall = error.config.url
+		// 			error.config.url = url
+		// 			$scope.parsedImages.push(error.config)
+		// 			$scope.parsedImages.sort(function(a,b) { return a.id - b.id; })
+		// 		});	
+
+		// })
+	}
+
+
+	//open function
+	$scope.recursiveFetch = function(position){
+		if(position < $scope.listToParse.length){
+
+			var imageAddress = $scope.listToParse[position]
+			var url = imageAddress
+			var i = position
+
 			var callString = "http://131.175.56.235:8080/single?img="+imageAddress+"&k=5"
+
+			// console.log(position,'\n',callString)
 
 			$http.get(callString)
 				.then(function(response){
@@ -57,8 +100,13 @@ angular.module('kromotologyApp')
 						$scope.showElements.download = true
 						$scope.showElements.go = false
 					}
+					// the function calls itself here in order to not process multiple asyncronous calls at the same time
+					// this is because the server fail to process some images due to the large number of simultaneous operations
+					// console.log(position, 'success')
+					position++;
+					$scope.recursiveFetch(position)
 				}, function(error){
-					console.log('error for this url: ', url)
+					console.log('error for the no. '+(1*position+1)+': ', url, '\n', callString)
 					$scope.errors++
 					error.config.id = i
 					error.config.parsed = false
@@ -66,10 +114,24 @@ angular.module('kromotologyApp')
 					error.config.url = url
 					$scope.parsedImages.push(error.config)
 					$scope.parsedImages.sort(function(a,b) { return a.id - b.id; })
+					// the function calls itself here in order to not process multiple asyncronous calls at the same time
+					// this is because the server fail to process some images due to the large number of simultaneous operations
+					// console.log(position, 'fail')
+					position++;
+					$scope.recursiveFetch(position)
 				});	
-
-		})
+		} else {
+			console.log('all done')
+			console.log($scope.parsedImages)
+		}
 	}
+
+	$scope.retryErrors = function(images) {
+		
+	}
+
+
+	//close function
 
 	$scope.getSVG = function(){
 	    document.getElementById("chart").innerHTML = "";
